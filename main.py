020269e6676ce7d23b0c209a1e2c2d21f80afcef
Henry Anorue <hanorue@drohealth.com>
2026-01-20 23:55:26 +0100

    Create a simple function to trace

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
     ConsoleSpanExporter,
 )
-import time
 
+
+import time
 
 provider = TracerProvider()
 processor = BatchSpanProcessor(ConsoleSpanExporter())
@@ -18,4 +19,14 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer("my.tracer.name")
 
 
+def process_order(order_id: int):
+    with tracer.start_as_current_span("process_order") as span:
+        span.set_attribute("order.id", order_id)
 
+        # Simulate work
+        time.sleep(1)
+
+        total = calculate_price(order_id)
+        span.set_attribute("order.total", total)
+
+        return total
