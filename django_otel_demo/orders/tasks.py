59852dbfdead65b5a7d0412339a949e9e6e8caa2
Henry Anorue <hanorue@drohealth.com>
2026-01-24 15:29:23 +0100

    Record celery task duration

--- django_otel_demo/orders/tasks.py
+++ django_otel_demo/orders/tasks.py
@@ -1,11 +1,14 @@
 import time
 from celery import shared_task
 from opentelemetry import trace
+from .metrics import celery_task_duration
 
 tracer = trace.get_tracer("orders.tasks")
 
 @shared_task
 def process_order_task(order_id: int):
+    start_time = time.time()
+
     with tracer.start_as_current_span("celery.process_order") as span:
         span.set_attribute("order.id", order_id)
 
@@ -14,4 +17,12 @@ def process_order_task(order_id: int):
         total = order_id * 100
         span.set_attribute("order.total", total)
 
-        return total
+    duration_ms = (time.time() - start_time) * 1000
+    celery_task_duration.record(
+        duration_ms,
+        attributes={
+            "task.name": "process_order_task"
+        }
+    )
+
+    return total
